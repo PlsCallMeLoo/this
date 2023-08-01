79be5ca5d3c712284dfc5701ca8e7fc12cc84951
Loo <plscallmeloo@gmail.com>
2023-08-01 10:29:13 +0100

    final

--- index.js
+++ index.js
@@ -3,7 +3,4 @@ $("button").click(() => $("h1").css("color", "green"))
 
 $("input").keypress((e) => $("h1").text(e.key))
 
-<<<<<<< HEAD
-window.close();master
-=======
->>>>>>> loo
+$("#closeButton").on("click",() => window.close())
---
+++ js.js
@@ -0,0 +1,3 @@
+function hideH1(){
+    $(".tit").toggle()
+}
